cdee2ce62ee9d1ca4e6575f2ddf4bd3e1e0ae997
spvb17 <saparalievb17@gmail.com>
2022-10-02 23:48:15 +0600

    commit before register

--- js/seriesthemechanger.js
+++ js/seriesthemechanger.js
@@ -3,19 +3,16 @@ let switchMode = document.getElementById("themeButton")
 switchMode.onclick = function()
 {
     let theme = document.getElementById("changer")
-    let themcss = document.getElementById("csschanger")
     let themeicon = document.getElementById("themeImages")
-    if(theme.getAttribute("href") == "css/draft.css" && themcss.getAttribute("href")=="css/lightslider.css" 
+    if(theme.getAttribute("href") == "css/serieslightmode.css"  
      && themeicon.getAttribute("src")=="img/15751.png")
     {
-        theme.href = "css/darkmode.css"
-        themcss.href="css/darkslider.css"
+        theme.href = "css/seriesdarkmode.css"
         themeicon.src="img/20221001_220021.png"
     }
     else
     {
-        theme.href  = "css/draft.css"
-        themcss.href = "css/lightslider.css"
+        theme.href  = "css/serieslightmode.css"
         themeicon.src= "img/15751.png"
     }
 }
